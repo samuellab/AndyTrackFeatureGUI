d0d3e6e30847b9ac4030fed7e8ab059c9e249b5d
Andrew Leifer <leifer@fas.harvard.edu>
2012-12-21 10:00:27 -0500

    pieces in place for defaulting to same brightness level or closest position to last frame

--- sample.m
+++ sample.m
@@ -1,5 +1,7 @@
+%This script is a sandbox for testing the TrackFeatureGUI
+
 %Get INformation
-basefilename='F:\DualMag\20120126\cex1_3_3\cex1_3_3t';
+basefilename='G:\DualMag\20111027\102711lgc55gc3_1_1_20111102_55046 PM\102711lgc55gc3_1_1_t';
 digits=4;
 extension='.tif';
 
@@ -7,6 +9,7 @@ minRange=1;
 maxRange=2000;
 
 findNeuronsInRed=true;
+radiusSmooth=4;
 
 %Load in the image info
 load([basefilename '_analysis.mat']);
@@ -14,7 +17,7 @@ imInfo=output.imInfo;
 
 loadFrame=getLoadFrameHandle(basefilename,extension,digits,imInfo,findNeuronsInRed,minRange,maxRange);
 
-findFeatures=getFindFeatureCandidatesHandle(5);
+findFeatures=getFindFeatureCandidatesHandle(5,radiusSmooth);
 
 [point,~]=BrightObjectTracker(loadFrame,findFeatures,minRange,maxRange);
 
