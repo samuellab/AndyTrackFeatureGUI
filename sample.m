67462d274ae21e4ce01247a6a5d4afe2b05d97a1
Andrew Leifer <leifer@fas.harvard.edu>
2013-01-09 11:17:53 -0500

    provided markers from other channel for temporally interleaved feature extraction

--- sample.m
+++ sample.m
@@ -1,24 +1,40 @@
 %This script is a sandbox for testing the TrackFeatureGUI
 
+
 %Get INformation
-basefilename='G:\DualMag\20111027\102711lgc55gc3_1_1_20111102_55046 PM\102711lgc55gc3_1_1_t';
+basefilename='G:\DualMag\20120220\lgc55_1_4\lgc55gc3_1_4t';
 digits=4;
 extension='.tif';
 
-minRange=1;
-maxRange=2000;
+minRange=710;
+maxRange=720;
 
 findNeuronsInRed=true;
 radiusSmooth=4;
 
 %Load in the image info
 load([basefilename '_analysis.mat']);
-imInfo=output.imInfo;
 
-loadFrame=getLoadFrameHandle(basefilename,extension,digits,imInfo,findNeuronsInRed,minRange,maxRange);
+greenChIs1=true;
+channelPrefix='c'
 
+    
+    %Load only the red channels
+    loadFrameRed=getLoadFrameHandleForAlternatingIllum(basefilename,...
+        extension,channelPrefix,digits,...
+        ~greenChIs1,greenChIs1,minRange,...
+        maxRange);
+    
+    %Load on the green channels
+    loadFrameGreen=getLoadFrameHandleForAlternatingIllum(basefilename,...
+        extension,channelPrefix,digits,...
+        greenChIs1,greenChIs1,minRange,...
+        maxRange);
+    
 findFeatures=getFindFeatureCandidatesHandle(5,radiusSmooth);
 
-[point,~]=BrightObjectTracker(loadFrame,findFeatures,minRange,maxRange);
+[pointGreen,~]=BrightObjectTracker(loadFrameGreen,findFeatures,minRange,maxRange);
 
+
+[pointRed,~]=BrightObjectTracker(loadFrameRed,findFeatures,minRange,maxRange,pointGreen);
 
